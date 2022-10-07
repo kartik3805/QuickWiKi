24f050ab78368fea60f525c1d6ef46afdcdabb0d
= <prathameshks2003@gmail.com>
2022-10-07 19:25:24 +0530

    added url verification and handled wrong request

--- script.js
+++ script.js
@@ -1,34 +1,51 @@
+document.getElementById('submitbtn').addEventListener('click', function () {
+    console.log(document.getElementById('url').value)
 
+    let inputurl = document.getElementById('url').value
+    let results_status = false;
+    let pagetitle;
+    if (inputurl == '') {
+        console.log("No url Given");
+        return;
+    }
+    try {
+        console.log(inputurl.replace(/^[^.]+\.wikipedia.org\/wiki\//g, ""))
+        pagetitle = inputurl.replace(/^[^.]+\.wikipedia.org\/wiki\//g, "")
+        results_status = true;
+    } catch (error) {
+        console.log(error);
+        document.querySelector('.content').style.visibility = 'hidden'
+        document.querySelector('.not-found').style.visibility = 'visible'
+        results_status = false;
+    }
 
+    if (results_status == true) {
+        fetch('https://en.wikipedia.org/api/rest_v1/page/summary/' + pagetitle)
+            .then(function (response) {
+                return response.json();
+            })
+            .then(function (json) {
+                // console.log(json)
+                try {
+                    document.querySelector('.image').innerHTML = `<img src="${json.thumbnail.source}" alt="">`
+                    document.querySelector('.page_title').innerHTML = `${json.title}`
+                    document.querySelector('.discription').innerHTML = `${json.description}`
+                    document.querySelector('.url_flag').innerHTML = `${json.content_urls.desktop.page}`
+                    document.querySelector('.url_flag').href = `${json.content_urls.desktop.page}`
+                    document.querySelector('.article').innerHTML = `${json.extract_html}`
+                    document.querySelector('.not-found').style.visibility = 'hidden'
 
+                    document.querySelector('.content').style.visibility = 'visible'
+                } catch (error) {
+                    document.querySelector('.content').style.visibility = 'hidden'
+                    document.querySelector('.not-found').style.visibility = 'visible'
 
-document.getElementById('submitbtn').addEventListener('click', function(){
-  console.log(document.getElementById('url').value)
-
-  let inputurl  = document.getElementById('url').value
-
-  console.log(inputurl.replace(/^[^.]+\.wikipedia.org\/wiki\//g, ""))
-  let pagetitle = inputurl.replace(/^[^.]+\.wikipedia.org\/wiki\//g, "")
-
-  fetch('https://en.wikipedia.org/api/rest_v1/page/summary/' + pagetitle)
-.then(function(response) {
-  return response.json();
-})
-.then(function(json) {
-console.log(json)
-
-document.querySelector('.image').innerHTML = `
-<img src="${json.thumbnail.source}" alt="">
-`
-document.querySelector('.page_title').innerHTML = `${json.title}`
-document.querySelector('.discription').innerHTML = `${json.description}`
-document.querySelector('.url_flag').innerHTML = `${json.content_urls.desktop.page}`
-document.querySelector('.url_flag').href = `${json.content_urls.desktop.page}`
-document.querySelector('.article').innerHTML = `${json.extract_html}`
-
-document.querySelector('.content').style.visibility = 'visible'
-
-})
+                }
+            })
+    } else {
+        document.querySelector('.content').style.visibility = 'hidden'
+        document.querySelector('.not-found').style.visibility = 'visible'
+    }
 
 })
 
