151afda3475770d931a9c424213380d4d508f414
Kartik <94182623+kartik3805@users.noreply.github.com>
2022-10-07 15:06:28 +0530

    Initial commit

---
+++ readme.md
@@ -0,0 +1,18 @@
+
+# QuickWiKi
+
+Get a quick and short summary for any wikipedia article in seconds
+
+## Inspiration
+
+When I was reading a wikipedia article I felt a need for a short summary of article with some basic details, which also can be shared to others so this is the reason I made this project.
+
+## Features
+
+- Get a short summary for any wikipedia article
+- Clean and modern look
+- More reader friendly
+
+## API
+
+Article data is derived from official wikipedia api : https://en.wikipedia.org/api/rest_v/
---
+++ script.js
@@ -0,0 +1,38 @@
+
+
+
+
+document.getElementById('submitbtn').addEventListener('click', function(){
+  console.log(document.getElementById('url').value)
+
+  let inputurl  = document.getElementById('url').value
+
+  console.log(inputurl.replace(/^[^.]+\.wikipedia.org\/wiki\//g, ""))
+  let pagetitle = inputurl.replace(/^[^.]+\.wikipedia.org\/wiki\//g, "")
+
+  fetch('https://en.wikipedia.org/api/rest_v1/page/summary/' + pagetitle)
+.then(function(response) {
+  return response.json();
+})
+.then(function(json) {
+console.log(json)
+
+document.querySelector('.image').innerHTML = `
+<img src="${json.thumbnail.source}" alt="">
+`
+document.querySelector('.page_title').innerHTML = `${json.title}`
+document.querySelector('.discription').innerHTML = `${json.description}`
+document.querySelector('.url_flag').innerHTML = `${json.content_urls.desktop.page}`
+document.querySelector('.url_flag').href = `${json.content_urls.desktop.page}`
+document.querySelector('.article').innerHTML = `${json.extract_html}`
+
+document.querySelector('.content').style.visibility = 'visible'
+
+})
+
+})
+
+
+
+
+//https://en.wikipedia.org/api/rest_v1/#/Page%20content/get_page_summary__title_
